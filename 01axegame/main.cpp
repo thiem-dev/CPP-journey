13fc2f2a17f8caf1fc96623e397ede2a8b80d393
Thiem Huynh <thiem.huynh0@gmail.com>
2024-02-08 00:02:49 -0500

    moving side to side circle noice

--- 01axegame/main.cpp
+++ 01axegame/main.cpp
@@ -1,14 +1,34 @@
 #include "raylib.h"
 int main()
 {
-  int width = 1000;
-  int height = 400;
+  // window dimensions
+  int width{1000};
+  int height{400};
   InitWindow(width, height, "first window");
 
+  // circle coordinates
+  int circle_x{175};
+  int circle_y{100};
+
+
 //WindowShouldClose() retuns true until X is clicked 
+  SetTargetFPS(60);
   while (WindowShouldClose() != true){
     BeginDrawing();
     ClearBackground(WHITE);
+
+    //Game logic begin
+
+    DrawCircle(circle_x, circle_y, 25, BLUE);
+
+    if(IsKeyDown(KEY_D)){
+      circle_x = circle_x + 10;
+    }
+    if(IsKeyDown(KEY_A)){
+      circle_x = circle_x - 10;
+    }
+
+    // GAME LOGIC END
     EndDrawing();
   }
 }
